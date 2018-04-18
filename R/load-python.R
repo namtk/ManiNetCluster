fbf12f2227ab2ca0bff1fb94c5c7f18833a5a251
Nam Nguyen <ndnguyen@cs.stonybrook.edu>
2018-04-18 19:19:15 +0000

    Modify load-python.R to put all Python Functions into Namespace

--- R/load-python.R
+++ R/load-python.R
@@ -1,23 +1,32 @@
-# Load the module and create dummy objects from it, all of which are NULL
-neighborhood <- reticulate::import_from_path(
-  "neighborhood",
-  file.path("inst", "python")
-)
-for (obj in names(neighborhood)) {
-  assign(obj, NULL)
-}
-# Clean up
-rm(neighborhood)
+library(reticulate)
+use_python("/home/dnguyen/anaconda3/bin/python")
 
-# Now all those names are in the namespace, and ready to be replaced on load
-.onLoad <- function(libname, pkgname) {
-  neighborhood <- reticulate::import_from_path(
-    "neighborhood",
-    system.file("python", package = packageName()),
-    delay_load = TRUE
+nmspaceLoad <- function(name) {
+  # Load the module and create dummy objects from it, all of which are NULL
+  py_module <- reticulate::import_from_path(
+    name,
+    file.path("inst", "python")
   )
-  # assignInMyNamespace(...) is meant for namespace manipulation
-  for (obj in names(neighborhood)) {
-    assignInMyNamespace(obj, neighborhood[[obj]])
+  for (obj in names(py_module)) {
+    assign(obj, NULL)
+  }
+  # Clean up
+  rm(py_module)
+  
+  # Now all those names are in the namespace, and ready to be replaced on load
+  .onLoad <- function(libname, pkgname) {
+    py_module <- reticulate::import_from_path(
+      name,
+      system.file("python", package = packageName()),
+      delay_load = TRUE
+    )
+    # assignInMyNamespace(...) is meant for namespace manipulation
+    for (obj in names(py_module)) {
+      assignInMyNamespace(obj, py_module[[obj]])
+    }
   }
 }
+
+allfilenames <- c("kmedoids", "distance", "neighborhood", "util", "alignment", "correspondence",
+              "dtw", "embedding", "viz", "warping")
+lapply(allfilenames, nmspaceLoad)
